194675bb60aa4a22621eba5e1b998afd48ed11f9
Joshua Westgard <jwestgard@mac.com>
2019-11-19 14:48:18 -0500

    first working accession record reader

--- accession/__main__.py
+++ accession/__main__.py
@@ -3,26 +3,37 @@
 import os
 import sys
 
+from records import AccessionRecord
+from records import AccessionRecordList
+
 
 def read_file(filepath):
-    '''Attempt to read the file path using various encodings and return
-        file contents'''
-    if not os.path.isfile(filepath):
-        print(f'Could not access {filepath}')
-        return None
-    else:
+    """
+    Read the file at the provided path, attempting with various encodings and return contents as a list of lines, with newlines removed, or None if the
+    file could not be read.
+    """
+    if os.path.isfile(filepath):
         for encoding in ['ascii', 'utf-8', 'latin-1']:
             try:
                 with open(filepath, encoding=encoding) as handle:
-                    contents = [line.strip() for line in handle.readlines()]
-                    return contents
+                    lines = [line.strip('\n') for line in handle.readlines()]
+                    return lines
             except ValueError:
+                # File decode unsuccessful, attempt next encoding
                 continue
+        # If all encodings have failed to decode the file
+        return None
+    else:
+        # If the file could not be found, or the path did not point to a file
+        print(f'Could not access {filepath}')
         return None
 
 
 def main():
-
+    """
+    Read the contents of each file in ROOT, listing the file and number of
+    lines.
+    """
     ROOT = sys.argv[1]
     files = os.listdir(ROOT)
     asset_total = 0
@@ -30,16 +41,54 @@ def main():
 
     for file in files:
         path = os.path.join(ROOT, file)
-        contents = read_file(path)
-        if contents is not None:
-            print(file, len(contents))
-            asset_total += len(contents)
+        if file.startswith('KAP'):
+            archive = 'KAP'
+        else:
+            archive = 'Archive' + file[7:10]
+        source = file
+        lines = read_file(path)
+        if lines is not None:
+            asset_total += len(lines)
+            firstline = lines[0]
+
+            if firstline.startswith('Volume in drive'):
+                type = 'dirlisting'
+                arlist = AccessionRecordList.from_dirlist(
+                                                lines, archive, source
+                                                )
+            elif firstline.startswith('2012-03-16-digital'):
+                type = 'special'
+                arlist = AccessionRecordList.from_special(
+                                                lines, archive, source
+                                                )
+            elif '\t' in firstline:
+                type = 'tab-delimited'
+                arlist = AccessionRecordList.from_csv(
+                                                lines, '\t', archive, source
+                                                )
+            elif ',' in firstline:
+                type = 'comma-delimited'
+                arlist = AccessionRecordList.from_csv(
+                                                lines, ',', archive, source
+                                                )
+            else:
+                type = 'unknown'
+
+            print()
+            print(f'   FILE: {file}')
+            print(f'   HEAD: {firstline}')
+            print(f'   TYPE: {type}')
+            print(f' SOURCE: {source}')
+            print(f'ARCHIVE: {archive}')
+            print()
+
         else:
             print(f'Failed to read {file}')
             error_total += 1
 
     print(f'Read {asset_total} lines from {len(files)} inventory files')
     print(f'{error_total} files could not be read')
+
 
 if __name__ == "__main__":
     main()
---
+++ accession/records.py
@@ -0,0 +1,99 @@
+import csv
+import os
+import sys
+
+
+class AccessionRecord():
+    """
+    Class representing the authoritative record of a digital asset
+    under management, usually created at accession time.
+    """
+
+    def __init__(self, filepath):
+        self.filename = os.path.basename(filepath)
+
+
+class AccessionRecordList():
+    """
+    Class representing a group of AccessionRecords.
+    """
+
+    def __init__(self, lines):
+        self.lines = lines
+
+    @classmethod
+    def from_dirlist(cls, lines, archive, source):
+        '''Read assets from a basic directory listing.'''
+        assets = []
+        for n, line in enumerate(lines):
+            if line == '' or line.startswith(' '):
+                continue
+            parts = line.split()
+            print(parts)
+            length = len(parts)
+            if length == 0 or parts[3] == '<DIR>':
+                continue
+            elif length >= 5:
+                timestamp = ' '.join(parts[:3])
+                bytes = int(''.join(
+                    [char for char in parts[3] if char.isdigit()]
+                    ))
+                filename = ' '.join(parts[4:])
+                assets.append((filename, bytes, timestamp))
+        #print(assets)
+
+    @classmethod
+    def from_special(cls, lines, archive, source):
+        '''Read special dirlist format.'''
+        assets = []
+        for n, line in enumerate(lines):
+            if line.startswith(' '):
+                parts = line.split('\t')
+                filename = parts[0].strip()
+                val, units = parts[2].strip().split()
+                if units == 'MB':
+                    bytes = int(round(float(val) * 1024 * 1024))
+                else:
+                    bytes = None
+                timestamp = parts[4].strip()
+                md5 = None
+                assets.append((filename, bytes, timestamp))
+        for asset in assets:
+            print(asset)
+
+    @classmethod
+    def from_csv(cls, lines, delimiter, archive, source):
+        '''Read assets from a CSV file.'''
+        assets = []
+        reader = csv.DictReader(lines, delimiter=delimiter)
+
+        for n, row in enumerate(reader):
+            bytes = None
+            timestamp = None
+            md5 = None
+            for key in ['Filename', 'FILENAME', 'File Name', 'Key']:
+                if key in row:
+                    filename = row[key]
+                    break
+                else:
+                    continue
+            for key in ['Bytes', 'BYTES', 'File Size', 'Size']:
+                if key in row:
+                    bytes = row[key]
+                    break
+                else:
+                    continue
+            for key in ['Mtime', 'MTime', 'MTIME', 'Mod Date', 'Date']:
+                if key in row:
+                    timestamp = row[key]
+                    break
+                else:
+                    continue
+            for key in ['MD5', 'md5', 'Data', 'Other']:
+                if key in row:
+                    md5 = row[key]
+                    break
+                else:
+                    continue
+            assets.append((filename, bytes, timestamp))
+        #print(assets)
